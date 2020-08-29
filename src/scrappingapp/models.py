b0aabece0145ec2d61e588a28a72919e53e5bd54
aizavgorodniy <azav@ukr.net>
2020-08-29 20:54:10 +0300

    29 less

--- src/scrappingapp/models.py
+++ src/scrappingapp/models.py
@@ -1,12 +1,55 @@
 from django.db import models
 
+from scrappingapp.utils import from_cyrillic_to_eng
+
+
 class City(models.Model):
 	name = models.CharField(max_length=50,
-							verbose_name='Название населенного пункта')
-	slug = models.CharField(max_length=50, blank=True)
+							verbose_name='Название населенного пункта',
+							unique=True)
+	slug = models.CharField(max_length=50, blank=True, unique=True)
 
 	class Meta:
 		verbose_name= 'Название населенного пункта'
 		verbose_name_plural= 'Название населенного пункта'
 	def __str__(self):
 		return self.name
+
+	def save(self, *args, **kwargs):
+		if not self.slug:
+			self.slug = from_cyrillic_to_eng(str(self.name))
+			super().save(*args, **kwargs)
+
+class Language(models.Model):
+	name = models.CharField(max_length=50,
+							verbose_name='Язык программирования',
+							unique=True)
+	slug = models.CharField(max_length=50, blank=True, unique=True)
+
+	class Meta:
+		verbose_name = 'Язык программирования'
+		verbose_name_plural = 'Языки программирования'
+
+	def __str__(self):
+		return self.name
+
+	def save(self, *args, **kwargs):
+		if not self.slug:
+			self.slug = from_cyrillic_to_eng(str(self.name))
+			super().save(*args, **kwargs)
+
+class Vacancy(models.Model):
+	url = models.URLField(unique=True)
+	title = models.CharField(max_length= 250, verbose_name='Название')
+	company = models.CharField(max_length=250, verbose_name='Компания')
+	description = models.TextField(verbose_name='Описание')
+	city = models.ForeignKey('City', on_delete= models.PROTECT, verbose_name='Город')
+	language = models.ForeignKey('Language', on_delete=models.PROTECT, verbose_name='Язык программирования')
+	timestamp = models.DateField(auto_now_add=True)
+
+	class Meta:
+		verbose_name = 'Вакансия'
+		verbose_name_plural = 'Вакансии'
+
+	def __str__(self):
+		return self.title
